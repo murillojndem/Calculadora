55f4c3c6efd6aad7ad3783049287881b893017cb
Murillo Master <murillojndem@gmail.com>
2020-11-18 11:20:48 -0300

    Criei a função dos botões da calculadora e implementei algumas funções da classe TCalcula

--- Unit1.dfm
+++ Unit1.dfm
@@ -20,6 +20,7 @@ object Form1: TForm1
     Height = 25
     Caption = '1'
     TabOrder = 0
+    OnClick = Button1Click
   end
   object Button2: TButton
     Left = 95
@@ -28,6 +29,7 @@ object Form1: TForm1
     Height = 25
     Caption = '2'
     TabOrder = 1
+    OnClick = Button2Click
   end
   object Button3: TButton
     Left = 176
@@ -36,6 +38,7 @@ object Form1: TForm1
     Height = 25
     Caption = '3'
     TabOrder = 2
+    OnClick = Button3Click
   end
   object Button4: TButton
     Left = 14
@@ -53,6 +56,7 @@ object Form1: TForm1
     Height = 25
     Caption = '5'
     TabOrder = 4
+    OnClick = Button5Click
   end
   object Button6: TButton
     Left = 176
@@ -61,6 +65,7 @@ object Form1: TForm1
     Height = 25
     Caption = '6'
     TabOrder = 5
+    OnClick = Button6Click
   end
   object Button7: TButton
     Left = 14
@@ -69,6 +74,7 @@ object Form1: TForm1
     Height = 25
     Caption = '7'
     TabOrder = 6
+    OnClick = Button7Click
   end
   object Button8: TButton
     Left = 95
@@ -77,6 +83,7 @@ object Form1: TForm1
     Height = 25
     Caption = '8'
     TabOrder = 7
+    OnClick = Button8Click
   end
   object Button9: TButton
     Left = 176
@@ -85,6 +92,7 @@ object Form1: TForm1
     Height = 25
     Caption = '9'
     TabOrder = 8
+    OnClick = Button9Click
   end
   object Button0: TButton
     Left = 95
@@ -93,20 +101,39 @@ object Form1: TForm1
     Height = 25
     Caption = '0'
     TabOrder = 9
+    OnClick = Button0Click
   end
   object Button11: TButton
-    Left = 304
+    Left = 288
     Top = 18
     Width = 75
     Height = 25
     Caption = '+'
     TabOrder = 10
+    OnClick = Button11Click
   end
   object edit_resultado: TEdit
-    Left = 432
-    Top = 32
-    Width = 89
-    Height = 73
+    Left = 400
+    Top = 18
+    Width = 145
+    Height = 41
+    Alignment = taCenter
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -27
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentFont = False
+    ReadOnly = True
     TabOrder = 11
+    Text = 'teste'
+  end
+  object Button10: TButton
+    Left = 432
+    Top = 95
+    Width = 75
+    Height = 25
+    Caption = '='
+    TabOrder = 12
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -20,7 +20,18 @@ type
     Button0: TButton;
     Button11: TButton;
     edit_resultado: TEdit;
+    Button10: TButton;
+    procedure Button9Click(Sender: TObject);
+    procedure Button11Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button0Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -38,9 +49,11 @@ type
     public
     constructor Create();
     procedure setNumero1 (snumero1 : integer);
+    function getNumero1 : double;
     procedure setNumero2 (snumero2 : integer);
     procedure setSinal   (ssinal   : char);
-    function Calculo (cnumero1, cnumero2 : integer; csinal : char);
+    function getSinal : char;
+    function Calculo : double;
   end;
 
 
@@ -50,11 +63,186 @@ type
 
 var
   Form1: TForm1;
+  calculo: TCalculo;
 
 implementation
 
 {$R *.dfm}
 
 
+
+
+//Implementação das funções da classe TCalculo / TCalculo functions implementation
+
+function TCalculo.Calculo: double;
+begin
+
+end;
+
+constructor TCalculo.Create;
+begin
+  fnumero1 := null;
+  fnumero2 := null;
+  fsinal   := 'a';
+end;
+
+function TCalculo.getNumero1: double;
+begin
+result := fnumero1;
+end;
+
+function TCalculo.getSinal: char;
+begin
+result := fsinal;
+end;
+
+procedure TCalculo.setNumero1(snumero1: integer);
+begin
+  fnumero1 := snumero1;
+end;
+
+procedure TCalculo.setNumero2(snumero2: integer);
+begin
+fnumero2 := snumero2;
+end;
+
+procedure TCalculo.setSinal(ssinal: char);
+begin
+fsinal := ssinal;
+end;
+
+// Fim da implementação das funções da classe TCalculo / TCalculo class functions implementation end
+
+//Implementação dos botões / Buttons Implementation
+
+procedure TForm1.Button0Click(Sender: TObject);
+begin
+  if calculo.getSinal = 'a' then
+  begin
+    calculo.setNumero1(0);
+    edit_resultado.text := floattostr(0);
+  end
+  else
+  begin
+    calculo.setNumero2(0);
+  end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(1);
+  end
+  else
+  begin
+    calculo.setNumero2(1);
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(2);
+  end
+  else
+  begin
+    calculo.setNumero2(2);
+  end;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(3);
+  end
+  else
+  begin
+    calculo.setNumero2(3);
+  end;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(4);
+  end
+  else
+  begin
+    calculo.setNumero2(4);
+  end;
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(5);
+  end
+  else
+  begin
+    calculo.setNumero2(5);
+  end;
+end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(6);
+  end
+  else
+  begin
+    calculo.setNumero2(6);
+  end;
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(7);
+  end
+  else
+  begin
+    calculo.setNumero2(7);
+  end;
+end;
+
+procedure TForm1.Button8Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(8);
+  end
+  else
+  begin
+    calculo.setNumero2(8);
+  end;
+end;
+
+procedure TForm1.Button9Click(Sender: TObject);
+begin
+  if calculo.getSinal = null then
+  begin
+    calculo.setNumero1(9);
+  end
+  else
+  begin
+    calculo.setNumero2(9);
+  end;
+end;
+
+// Implementação dos botões de operação / Operations button's implementation
+
+procedure TForm1.Button11Click(Sender: TObject);
+begin
+
+end;
+
+//Fim da implementação dos botões / Buttons Implementation end
 
 end.
